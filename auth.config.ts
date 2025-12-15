444c08a02f232b79d87ecc96bdcddf64dd000493
tirr-fidelaa <titouzumaki879@gmail.com>
2025-12-15 21:02:07 +0700

    fifth commit

--- auth.config.ts
+++ auth.config.ts
@@ -9,9 +9,11 @@ export const authConfig = {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
-        return isLoggedIn;
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
-
       return true;
     },
   },
